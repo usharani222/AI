5765d132765e0c7ccf373f4b1fc30b8674fea956
usharani222 <usha78633@gmail.com>
2026-09-21 12:39:47 +0530

    api use

---
+++ api.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+api_key=os.getenv("OPENROUTER_API_KEY")
+
+if not api_key:
+    raise RuntimeError("API Key missing")
+
+client=OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key
+)
+prompt=input("Enter prompt:")
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role":"user",
+            "content":prompt,
+        }
+    ],
+    extra_body={
+        "usage":{
+            "include":True
+        }
+    }
+)
+
+
+print("Raw response:")
+print(response)
+print("="*250+"\n")
+
+print("Usage")
+if response.usage:
+    print("Completion usage",response.usage.completion_tokens)
+    print("Prompt usage",response.usage.prompt_tokens)
+    print("Total usage",response.usage.total_tokens)
+
+
+print("\n" + "=" * 70)
+print("MODEL RESPONSE")
+print("=" * 70)
+
+print(response.choices[0].message.content)
